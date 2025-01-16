4f040f1dc5eb24217a69fc87277c5fa0b82cde7e
Felipe Afonso <fmunhozafonso@gmail.com>
2025-01-16 20:53:00 -0300

    feat: layout

--- pages/home/home_handler.go
+++ pages/home/home_handler.go
@@ -4,11 +4,17 @@ import (
 	"github.com/a-h/templ"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"go-htmx-starter/pages"
 )
 
 func HomeHandler(c *fiber.Ctx) error {
 	name := "John"
 	page := home(name)
-	template := templ.Handler(page)
+	template := templ.Handler(
+	pages.Layout(
+			"Go + HTMX starter",
+			page,
+			)
+		)
 	return adaptor.HTTPHandler(template)(c)
 }
