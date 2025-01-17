bf4c93be5a660cb430c4be502b8788fe5ce90363
Felipe Afonso <fmunhozafonso@gmail.com>
2025-01-17 15:13:33 -0300

    feat: added tailwind

--- dev.sh
+++ dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-watchexec -r -e go,templ,css,json,js,ts,html -d 1500ms -E DEV=true -- curl -X POST -s -o /dev/null http://localhost:3000/reload/new &
+watchexec -r -e go,templ,css,json,js,ts,html -d 2500ms -E DEV=true -- curl -X POST -s -o /dev/null http://localhost:3000/reload/new &
+watchexec -r -e templ,html -- tailwindcss -i ./pages/tailwind.css -o ./static/styles.css &
 air
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./static/**/*.{html,templ}", "./pages/**/*.{html,templ}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
