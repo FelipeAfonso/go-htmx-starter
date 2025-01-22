adf4d6abb4d1ba16114edf1d9485fc7f9bdf979c
Felipe Afonso <fmunhozafonso@gmail.com>
2025-01-22 16:59:40 -0300

    chore: cleanup

--- dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-tailwindcss -i ./pages/tailwind.css -o ./static/styles.css 
-templ generate 
-(sleep 2 && curl -X POST -s -o /dev/null http://localhost:3000/reload/new)&
-go run .
--- watch-dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-watchexec -r -e go,templ,css,json,js,ts,html -i _templ.go -d 1s -E DEV=true -- ./dev.sh
