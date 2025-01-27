c086e515321a71220f7dae927d5b66973f56dbb6
Felipe Afonso <fmunhozafonso@gmail.com>
2025-01-27 14:28:06 -0300

    feat: tailwind 4

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./pages/**/*.{html,templ}", "./assets/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
